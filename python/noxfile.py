5bae4f25acf784438c32bbd8c0ab56477b66a380
Lucina <lucina@lmbyrne.dev>
2022-12-26 05:37:06 +0000

    Allow format

--- python/noxfile.py
+++ python/noxfile.py
@@ -323,7 +323,7 @@ def copy_actions(_: nox.Session) -> None:
         loader=jinja2.FileSystemLoader(pathlib.Path(__file__).parent.parent / "github" / "actions"),
     )
 
-    env.filters["quoted"] = '"{}"'.format
+    env.filters["quoted"] = '"{}"'.format  # noqa: FS002
 
     to_write: dict[pathlib.Path, str] = {}
     if isinstance(_config.github_actions, dict):
