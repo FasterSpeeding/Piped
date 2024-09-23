e443e1528ce3ded94b353e4c74374593a89826fe
FasterSpeeding <lucina@lmbyrne.dev>
2024-09-23 22:33:56 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -66,13 +66,12 @@ if sys.version_info < (3, 11):
 
 _LOGGER = logging.getLogger("piped.bot")
 _LOGGER.setLevel("INFO")
-_HTTP_TIMEOUT = datetime.timedelta(minutes=10)
 
 dotenv.load_dotenv()
 
 _username = os.environ.get("CLIENT_NAME", default="always-on-duty") + "[bot]"
 
-with httpx.Client(timeout=_HTTP_TIMEOUT) as client:
+with httpx.Client() as client:
     _user_id = int(client.get(f"https://api.github.com/users/{_username}").json()["id"])
 
 APP_ID = os.environ["APP_ID"]
@@ -562,7 +561,7 @@ class _IterWorkflows:
 
 
 async def _on_startup():
-    app.state.http = httpx.AsyncClient(timeout=_HTTP_TIMEOUT)
+    app.state.http = httpx.AsyncClient()
     app.state.index = _ProcessingIndex()
     app.state.tokens = _Tokens()
     app.state.workflows = _WorkflowDispatch()
